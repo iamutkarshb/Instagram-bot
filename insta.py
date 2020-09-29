e37e72dad655abf691fe5139b6492c2d9f7691c9
keyboard-king <utkarshbansal01@gmail.com>
2020-09-30 00:05:44 +0530

    insta bot

---
+++ insta.py
@@ -0,0 +1,49 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+class InstagramBot:
+	def __init__(self,username,password):
+		self.username = username
+		self.password = password
+		self.bot = webdriver.Firefox(executable_path = 'C:/Users/Utkarsh/Downloads/geckodriver.exe')	
+
+	def login(self):
+		bot = self.bot
+		bot.get('https://www.instagram.com/accounts/login/')
+		time.sleep(5)
+		email = bot.find_element_by_name('username').send_keys(self.username)
+		password = bot.find_element_by_name('password').send_keys(self.password + Keys.RETURN)
+
+		time.sleep(5)
+
+	def searchHashtag(self,hashtag):
+		bot = self.bot
+
+		bot.get('https://www.instagram.com/explore/tags/' + hashtag)
+
+	def likePhotos(self,amount):
+		bot = self.bot
+
+		bot.find_element_by_class_name('v1Nh3').click()
+
+		i = 1
+		while i <= amount:
+			time.sleep(3)
+			bot.find_element_by_class_name('fr66n').click()
+			bot.find_element_by_class_name('coreSpriteRightPaginationArrow').click()
+			time.sleep(3)
+
+			i += 1
+
+		
+		bot.get('https://www.instagram.com/' + self.username)
+
+
+
+
+
+insta = InstagramBot('username', 'password')
+insta.login()
+insta.searchHashtag('photography')
+insta.likePhotos(50)
